7a9fc58c8bc591be036dd0ad89e1c5b8d434d90b
Skand Hurkat <skand_hurkat@yahoo.co.in>
2012-06-18 22:48:56 +0530

    added GNU GPL v3 to all files

--- CarDetect.cpp
+++ CarDetect.cpp
@@ -1,3 +1,22 @@
+//CarDetect.cpp
+//© Skand Hurkat, 2011
+
+/*     This file is part of CarDetect.
+ *
+ *     CarDetect is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     CarDetect is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with CarDetect.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* This is the main file in the program. What it does is listed as follows:
  * It accepts arguments from the user and loads the various files.
  * It then creates a thread for haarcascade classification (Viola and Jones)
