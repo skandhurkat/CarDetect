9a7edb9482d0a9b6de0315cc84a5ff82b3669e83
Skand Hurkat <skand_hurkat@yahoo.co.in>
2013-07-30 17:11:09 -0400

    CMake build system set up

---
+++ include/globals.h
@@ -0,0 +1,51 @@
+// globals.h
+// Copyright 2011, 2012 Skand Hurkat
+
+//  This file is part of CarDetect.
+//
+//  CarDetect is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  CarDetect is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with CarDetect.  If not, see <http://www.gnu.org/licenses/>.
+
+#ifndef GLOBALS_H_INCLUDED
+#define GLOBALS_H_INCLUDED
+
+#include <opencv2/objdetect/objdetect.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+
+#include <string>
+#include <vector>
+#include <windows.h>
+
+using namespace cv;
+using namespace std;
+
+extern double scale;
+
+extern int cascade_flags;
+
+extern CascadeClassifier cascade;
+
+extern Mat img;
+
+extern vector<Rect> haarrectangles;
+
+extern bool terminate_program;
+
+//mutexes for multithreading:
+extern HANDLE img_mutex;
+extern HANDLE haarrectangles_mutex;
+
+extern HANDLE haarthread;
+extern unsigned int haarthreadid;
+
+#endif // GLOBALS_H_INCLUDED
